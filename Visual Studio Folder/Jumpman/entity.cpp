79a0b7bd9df0e914980aedbe7e7204ddd02950c8
Kayne Ruse <ratstail91@yahoo.com.au>
2012-07-27 19:50:58 +1000

    Imported new Entity from Quick Start. Added tweaking parameters to GetWorldBBox()

--- Visual Studio Folder/Jumpman/entity.cpp
+++ Visual Studio Folder/Jumpman/entity.cpp
@@ -18,23 +18,44 @@ Entity::~Entity() {
 	//
 }
 
-void Entity::LoadImage(const char* szFileName) {
-	Image::LoadImage(szFileName);
+void Entity::LoadImage(const char* szFileName, Sint16 x, Sint16 y, Uint16 w, Uint16 h) {
+	Image::LoadImage(szFileName, x, y, w, h);
 
-	BBox2D::SetBBox( 0, 0, (float)GetImageW()-1, (float)GetImageH()-1 );
+	BBox2D::SetBBox( GetImageX(), GetImageY(), GetImageW(), GetImageH() );
+}
+
+void Entity::LoadImage(SDL_Surface* pSurface,  Sint16 x, Sint16 y, Uint16 w, Uint16 h) {
+	Image::LoadImage(pSurface, x, y, w, h);
+
+	BBox2D::SetBBox( GetImageX(), GetImageY(), GetImageW(), GetImageH() );
 }
 
 void Entity::Update(int iDelta) {
 	Origin2D::Update(iDelta);
 
-	SetImageX( (int)GetOriginX() );
-	SetImageY( (int)GetOriginY() );
+	Image::SetImageX( (Sint16)Origin2D::GetOriginX() );
+	Image::SetImageY( (Sint16)Origin2D::GetOriginY() );
 }
 
-BBox2D::Rect Entity::GetWorldBBox() {
-	return BBox2D::GetWorldBBox(GetOriginX(), GetOriginY());
+SDL_Rect Entity::GetWorldBBox(Sint16 x, Sint16 y, Sint16 w, Sint16 h) {
+	/* The parameters (x, y, w, h) shift that part of the box by so many
+	 * pixels. These are the "tweak" parameters, that temporarily alter
+	 * THIS entity's bounding box.
+	 * 
+	 * There is no error checking yet.
+	*/
+
+	SDL_Rect myBox = BBox2D::GetWorldBBox( (Sint16)Origin2D::GetOriginX(), (Sint16)Origin2D::GetOriginY());
+
+	//tweaks
+	myBox.x += x;
+	myBox.y += y;
+	if (myBox.w > -w) myBox.w += w; else myBox.w = 0;
+	if (myBox.h > -h) myBox.h += h; else myBox.h = 0;
+
+	return myBox;
 }
 
-int Entity::CheckWorldBBox(BBox2D::Rect box) {
-	return BBox2D::CheckWorldBBox(GetOriginX(), GetOriginY(), box);
+int Entity::CheckWorldBBox(SDL_Rect otherBox) {
+	return BBox2D::CheckWorldBBox(otherBox, (Sint16)Origin2D::GetOriginX(), (Sint16)Origin2D::GetOriginY());
 }
--- Visual Studio Folder/Jumpman/entity.h
+++ Visual Studio Folder/Jumpman/entity.h
@@ -17,12 +17,13 @@ public:
 	Entity();
 	virtual ~Entity();
 
-	virtual void LoadImage(const char* szFileName);
+	virtual void LoadImage(const char* szFileName, Sint16 x=0, Sint16 y=0, Uint16 w=0, Uint16 h=0);
+	virtual void LoadImage(SDL_Surface* pSurface,  Sint16 x=0, Sint16 y=0, Uint16 w=0, Uint16 h=0);
 
 	virtual void Update(int iDelta);
 
-	virtual Rect GetWorldBBox	();
-	virtual int CheckWorldBBox	(Rect box);
+	virtual SDL_Rect GetWorldBBox	(Sint16 x=0, Sint16 y=0, Sint16 w=0, Sint16 h=0);
+	virtual int CheckWorldBBox		(SDL_Rect otherBox);
 
 protected:
 	/* Protected access members */
